38e2aa36138028ee87784f180c7a24727c8bc9c3
TheNav04 <ghummannavtej@gmail.com>
2026-04-03 11:38:01 -0400

    Fixed Server Error Plus added new data

---
+++ Backend/dbHandler.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+//IMPORTANT
+//Make a dbconfig.js file in the Backend
+//This file will not be on git because of git ignore
+const {
+    userName,
+    hostType,
+    databaseName,
+    databasePass,
+    port
+  } = require('./dbConfig.js');
+
+//THIS INFORMATION MUST BE CHANGED DEPENDNING ON WHO IS RUNNING THE DB
+const pool = new Pool({
+  user: userName,   //Can find username when creating a connection. (Above the folder icon in postgre)
+  host: hostType, //project is local hosted
+  database: databaseName, //Can find the db name when creating a connection
+  password: databasePass, //If you dont remember your password just reset it
+  port: port,   //run: SHOW port inside postgre to find it, default is 5432
+});
+
+module.exports = pool; 
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,9 @@
     //Node packages are not stored on git, redownload them through terminal
     //npm install must be run for both Frontend and Backend
 
+//What to install in backend
+    //npm install 
+
 //How to run
     //In terminal tab create two terminals
     //One should CD into Frontend
@@ -9,15 +12,52 @@
         //Frontend terminal runs npm run dev
         //backend terminal runs node server.js
 
+console.log("LOADED THIS SERVER FILE");
+
+const pool = require('./dbHandler.js');
+const path = require('path');
+const bcrypt = require('bcrypt');
 const express = require('express');
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'nodeJS express backend is online.' });
+app.get('/', (req, res) => {
+  res.send('server is alive');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+//Test by making component do an api call
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'nodeJS express backend is online.' });
+});
+
+
+//to test return do http://localhost:3000/api/testDbConnection
+app.get('/api/testDbConnection', async (req, res) => {
+    try{
+        const data = await pool.query('select count(*) AS Working from staff');
+        res.json(data.rows); 
+        // Will Return {command: 'SELECT', rowCount: 2, oid: null, rows: Array(2), fields: Array(3), …} 
+        //rows mean data, fields mean attributes
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+    
+})
+
+
+
+const server = app.listen(3000, () => {
+    console.log('Server running on port 3000. run the frontend dev view'); 
+
+    //backend turns off without this. DONT REMOVE.
+    setInterval(() => {
+        console.log('alive');
+    }, 3000);
+
+    //server wasnt working because another process was runnin in the back while hidden on port 3000
+    //THE FIX: npx kill-port 3000
 });
